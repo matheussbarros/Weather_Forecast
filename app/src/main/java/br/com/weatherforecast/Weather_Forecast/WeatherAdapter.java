51f9eefd4d9f7246b078ee33cb1021cb395fd01e
Matheus <matheusdsbarros@gmail.com>
2019-05-09 09:39:56 -0300

    Criação do LinearLayout,Class Weather e WeatherAdapter

---
+++ app/src/main/java/br/com/weatherforecast/Weather_Forecast/WeatherAdapter.java
@@ -0,0 +1,20 @@
+package br.com.weatherforecast.Weather_Forecast;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.ArrayAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeatherAdapter extends ArrayAdapter <Weather> {
+
+    private Map <String, Bitmap> figuras = new HashMap<>();
+
+    public WeatherAdapter(Context context, List<Weather> previsoes){
+        super(context,-1,previsoes);
+
+    }
+
+}

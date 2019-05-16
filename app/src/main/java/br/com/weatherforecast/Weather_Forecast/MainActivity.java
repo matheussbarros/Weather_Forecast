a5359466924b06fe093a950163619dd6d1611306
Matheus <matheusdsbarros@gmail.com>
2019-05-16 09:47:24 -0300

    Conectando com a API

--- app/src/main/java/br/com/weatherforecast/Weather_Forecast/MainActivity.java
+++ app/src/main/java/br/com/weatherforecast/Weather_Forecast/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.weatherforecast.Weather_Forecast;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,25 +9,152 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText locationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationEditText = findViewById(R.id.locationEditText);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        FloatingActionButton fab =  findViewById(R.id.fab);
+        fab.setOnClickListener((v)->{
+            String cidade = locationEditText.getEditableText().toString();
+            String endereco = getString(
+                    R.string.web_service_url,
+                    cidade,
+                    getString(R.string.api_key)
+            );
+            obtemPrevisoesV3(endereco);
         });
     }
+
+    public void obtemPrevisoesV4(String endereco){
+        new ObtemPrevisoes().execute(endereco);
+    }
+
+    class ObtemPrevisoes extends AsyncTask<String, Void, String>{
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                URL url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+                BufferedReader reader = new BufferedReader(isr);
+                StringBuilder sb = new StringBuilder("");
+                String linha = null;
+                while((linha = reader.readLine()) != null)
+                    sb.append(linha);
+                return sb.toString();
+
+            }catch(IOException e){
+                e.printStackTrace();
+                return  null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(MainActivity.this,s.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    public void obtemPrevisoesV3(String endereco){
+
+        new Thread(()->{
+            try{
+                URL url = new URL(endereco);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+                BufferedReader reader = new BufferedReader(isr);
+                StringBuilder sb = new StringBuilder("");
+                String linha = null;
+                while((linha = reader.readLine()) != null)
+                    sb.append(linha);
+
+                //Enfilera na fila para ser executada no componente principal
+                runOnUiThread(()->{
+                    Toast.makeText(this,sb.toString(), Toast.LENGTH_SHORT).show();
+                });
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }).start();
+
+    }
+
+
+    public void obtemPrevisoesV2(String endereco){
+
+        new Thread(()->{
+            try{
+                URL url = new URL(endereco);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+                BufferedReader reader = new BufferedReader(isr);
+                StringBuilder sb = new StringBuilder("");
+                String linha = null;
+                while((linha = reader.readLine()) != null);
+                Toast.makeText(this,sb.toString(), Toast.LENGTH_SHORT).show();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
+
+
+    /*
+    * Este código não funciona porque esta sendo chamado na thread principal,
+    * e thread principal é responsável por atualizar a interface  grafica, fazendo isso congelara a tela do usuario
+    *
+    * Thred principal não pode fazer requisições externas
+    *
+    * Em uma Thred diferente da principal não é possivel interagir com os componentes visuais
+    *
+    * NetworkOnMainThreadException ->
+    *
+    * */
+    public void obtemPrevisoesV1(String endereco){
+
+        try{
+            URL url = new URL(endereco);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder("");
+            String linha = null;
+            while((linha = reader.readLine()) != null);
+            Toast.makeText(this,sb.toString(), Toast.LENGTH_SHORT).show();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
